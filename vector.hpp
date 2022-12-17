d0fee6ab0bbc77748c77c31f9a82d291b0cdd808
Yann BELLOT <yannbellot@yahoo.fr>
2022-12-17 23:55:57 +0100

    ajout typename

--- vector.hpp
+++ vector.hpp
@@ -4,10 +4,29 @@
 #include <iostream>
 #include <vector>
 
-
-class vector
+namespace ft
 {
+	template<typename T,typename Alloc>
+	class vector
+	{
+		typedef T										value_type
+		typedef Allocator								allocator_type
+		typedef std::size_t								size_type
+		typedef std::ptrdiff_t							difference_type
+		typedef value_type&								reference
+		typedef	const value_type&						const_reference
+		typedef Allocator::pointer						pointer
+		typedef Allocator::const_pointer				const_pointer
+		//typedef iterator
+		//typedef const_iterator
+		typedef std::reverse_iterator<iterator>			reverse_iterator
+		typedef std::reverse_iterator<const_iterator>	const_reverse_iterator
 
-};
+		public:
+
+		private:
+		
+	};
+}
 
 #endif
